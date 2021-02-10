119a7c8ffd2fcc5d379981c532870110d02275b0
Saddam <48236904+ZizouQ-Q@users.noreply.github.com>
2021-02-10 13:39:54 +0100

    initial

---
+++ pages/about.js
@@ -0,0 +1,20 @@
+import Head from "next/head";
+
+const About = () => {
+    return ( 
+        <>
+            <Head>
+            <title>Ninja List | About</title>
+            <meta name="keywords" content="ninjas, about"/>
+            </Head>
+            <div>
+                <h1>About</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod nemo voluptatibus tenetur quam nam sapiente numquam incidunt repellendus eos porro?</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod nemo voluptatibus tenetur quam nam sapiente numquam incidunt repellendus eos porro?</p>
+                
+            </div>
+        </>
+     );
+}
+ 
+export default About;
